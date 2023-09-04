69939fdeeab5df862d3d51e00d383bb5144bdc40
SotaSakuragi <ebi.ebi.kani.kani.ikura@gmail.com>
2023-09-04 13:18:47 +0900

    完成

--- MarubatuApp/SecondViewController.swift
+++ MarubatuApp/SecondViewController.swift
@@ -10,52 +10,77 @@ import UIKit
 class SecondViewController: UIViewController {
     var questions: [[String: Any]] = []
     var answer = false
-
+    
+    
     @IBOutlet weak var textView: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let userDefaults = UserDefaults.standard
+        //"add"というキーで保存された値がなにかある -> 値をtaskArrayへ
+        if userDefaults.object(forKey: "add") != nil {
+            questions = userDefaults.object(forKey: "add") as! [[String: Any]]
+        }
+        
         // Do any additional setup after loading the view.
     }
     
-
+    
     @IBAction func topButton(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
     
     @IBAction func marubatuSeg(_ sender: Any) {
         switch (sender as AnyObject).selectedSegmentIndex {
-            case 0:
-                answer = false
-            case 1:
-                answer = true
-            default:
-                break
+        case 0:
+            answer = false
+        case 1:
+            answer = true
+        default:
+            break
         }
     }
     
     @IBAction func setButton(_ sender: Any) {
-        if let newText = textView.text {
-        let newQuestions: [String: Any] = ["question": newText]
-            questions.append(newQuestions)
-            questions.append(["answer": answer])
-            textView.text = ""
-            print(questions)
+        if textView.text != "" {
+            if let newText = textView.text {
+                var newQuestions: [String: Any] = [:]
+                newQuestions["question"] = newText
+                newQuestions["answer"] = answer
+                questions.append(newQuestions)
+                textView.text = ""
+                print(questions)
+                let userDefaults = UserDefaults.standard
+                userDefaults.set(questions, forKey: "add")
+            }
+        } else {
+            showAlert(title: "空です", message: "問題が入力されてません")
         }
         
     }
     
     @IBAction func deleteButton(_ sender: Any) {
-        questions.isEmpty
+        questions = []
+        let userDefaults = UserDefaults.standard
+        userDefaults.set(questions, forKey: "add")
+        showAlert(title: "削除！！", message: "問題が削除されました")
+        
     }
+    
+    func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert) // 表示作成
+        let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)           // ボタン作成
+        alert.addAction(alertAction)                                                          // 表示にボタンを追加
+        present(alert, animated: true)                                                        // 画面に表示
+    }
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
--- MarubatuApp/ViewController.swift
+++ MarubatuApp/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var questionLabel: UILabel!
     
     var currentQuestionNum: Int = 0
@@ -21,17 +21,23 @@ class ViewController: UIViewController {
             if let que = question["question"] as? String {
                 questionLabel.text = que
             }
+        } else {
+            questionLabel.text = "問題がないよ、入力してね"
+            print("あああ")
         }
+        
     }
     
     func checkAnswer(yourAnswer: Bool) {
-        let question = questions[currentQuestionNum]
-        if let ans = question["answer"] as? Bool {
-            if yourAnswer == ans {
-                currentQuestionNum += 1
-                showAlert(message: "正解")
-            } else {
-                showAlert(message: "不正解")
+        if !questions.isEmpty {
+            let question = questions[currentQuestionNum]
+            if let ans = question["answer"] as? Bool {
+                if yourAnswer == ans {
+                    currentQuestionNum += 1
+                    showAlert(message: "正解")
+                } else {
+                    showAlert(message: "不正解")
+                }
             }
         }
         
@@ -50,10 +56,37 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //        let userDefaults = UserDefaults.standard
+        //        //"add"というキーで保存された値がなにかある -> 値をtaskArrayへ
+        //        if userDefaults.object(forKey: "add") != nil {
+        //            questions = userDefaults.object(forKey: "add") as! [[String: Any]]
+        //        }
         showQuestion()
         // Do any additional setup after loading the view.
     }
-
+    
+    //    override func loadView() {
+    //        super.loadView()
+    //        let userDefaults = UserDefaults.standard
+    //        //"add"というキーで保存された値がなにかある -> 値をtaskArrayへ
+    //        if userDefaults.object(forKey: "add") != nil {
+    //            questions = userDefaults.object(forKey: "add") as! [[String: Any]]
+    //        }
+    //    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let userDefaults = UserDefaults.standard
+        //"add"というキーで保存された値がなにかある -> 値をtaskArrayへ
+        if userDefaults.object(forKey: "add") != nil {
+            questions = userDefaults.object(forKey: "add") as! [[String: Any]]
+            print("dflghfdklg")
+            print(questions)
+            print("jhkfjgdg")
+            showQuestion()
+        }
+    }
+    
     @IBAction func tappedNoButton(_ sender: UIButton) {
         checkAnswer(yourAnswer: false)
     }
